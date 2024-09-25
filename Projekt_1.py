047e6966301dee101e22e44b8922c4898cdb57f6
Veronika Svitakova <svitakovaveronika16@gmail.com>
2024-09-25 19:50:51 +0200

    First version of project

---
+++ Projekt_1.py
@@ -0,0 +1,89 @@
+"""
+projekt_1.py: první projekt do Engeto Online Python Akademie
+
+author: Veronika Svitáková
+email: svitakovaveronika16@gmail.com
+discord: veronikasvitakova
+"""
+
+users = {"bob": "123", "ann": "pass123", "mike": "password123", "liz": "pass123" }
+
+texts = ['''
+Situated about 10 miles west of Kemmerer,
+Fossil Butte is a ruggedly impressive
+topographic feature that rises sharply
+some 1000 feet above Twin Creek Valley
+to an elevation of more than 7500 feet
+above sea level. The butte is located just
+north of US 30N and the Union Pacific Railroad,
+which traverse the valley. ''',
+'''At the base of Fossil Butte are the bright
+red, purple, yellow and gray beds of the Wasatch
+Formation. Eroded portions of these horizontal
+beds slope gradually upward from the valley floor
+and steepen abruptly. Overlying them and extending
+to the top of the butte are the much steeper
+buff-to-white beds of the Green River Formation,
+which are about 300 feet thick.''',
+'''The monument contains 8198 acres and protects
+a portion of the largest deposit of freshwater fish
+fossils in the world. The richest fossil fish deposits
+are found in multiple limestone layers, which lie some
+100 feet below the top of the butte. The fossils
+represent several varieties of perch, as well as
+other freshwater genera and herring similar to those
+in modern oceans. Other fish such as paddlefish,
+garpike and stingray are also present.'''
+]
+def login():
+    #overeni zda uzivatel je registrovany
+    username = input("username: ")
+    password = input("password: ")
+    
+    if username in users and users[username] == password:
+        print(f"----------------------------------------\nWelcome to the app, {username}")
+        print("We have 3 texts to be analyzed.")
+        return True
+    else:
+        print("unregistered user, terminating the program..")
+        return False
+    
+def analyze_text(text):
+    words = text.split()
+    word_count = len(words)
+    titlecase_words = [word for word in words if word.istitle()]
+    uppercase_words = [word for word in words if word.isupper() and word.isalpha()]
+    lowercase_words = [word for word in words if word.islower()]
+    numeric_strings = [word for word in words if word.isdigit()]
+    sum_of_numbers = sum([int(num) for num in numeric_strings])
+
+    print(f"There are {word_count} words in the selected text.")
+    print(f"There are {len(titlecase_words)} titlecase words.")
+    print(f"There are {len(uppercase_words)} uppercase words.")
+    print(f"There are {len(lowercase_words)} lowercase words.")
+    print(f"There are {len(numeric_strings)} numeric strings.")
+    print(f"The sum of all the numbers {sum_of_numbers}")
+
+    word_lengths = {}
+    for word in words:
+        length = len(word.strip(".,!?")) 
+        word_lengths[length] = word_lengths.get(length, 0) + 1
+
+    print("----------------------------------------")
+    print("LEN|  OCCURENCES   |NR.")
+    print("----------------------------------------")
+    for length, occurrence in sorted(word_lengths.items()):
+        print(f"{length:>3}|{'*' * occurrence:<15}|{occurrence}")
+
+if login():
+    try:
+        text_choice = int(input("Enter a number btw. 1 and 3 to select: "))
+        if text_choice < 1 or text_choice > 3:
+            print("Invalid choice, terminating the program.")
+        else:
+            print("----------------------------------------")
+            analyze_text(texts[text_choice - 1])
+            print("----------------------------------------")
+    except ValueError:
+        print("Invalid input, terminating the program..")
+
